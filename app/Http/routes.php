2a009268ecbdfecf684b59b2a505e1f0d2ec3204
Gigabyte 8gn <akram.cse007@gmail.com>
2019-04-24 07:44:36 +0600

    upto ORM

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Post;
 /*
 |--------------------------------------------------------------------------
 | Routes File
@@ -10,6 +10,8 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
@@ -38,13 +40,134 @@ Route::get('/', function () {
 
 //Route::get('/post','PostsController@index');
 
-Route::resource('posts','PostsController');
+//Route::resource('posts','PostsController');
+//
+//
+//Route::get('/contact','PostsController@contact');
+//
+//
+//Route::get('/post/{id}','PostsController@showPost');
 
 
-Route::get('/contact','PostsController@contact');
+/*
+ -----------------------------------------------------------------------------
+ Database CRUD
+-------------------------------------------------------------------------------
+ */
+
+// Insert Operation
+
+//Route::get('/insert', function () {
+//
+//    DB::insert('insert into posts(title,content) values(?,?)', ['Laravel','Laravel is most using php framework']);
+//
+//});
+//
+// //Read Operation
+//
+//Route::get('/read', function () {
+//
+//   $results = DB::select('select * from posts where id=?',[1]);
+//
+//    foreach($results as $posts)
+//    {
+//            return $posts->title;
+//    }
+//
+//});
+//
+//
+//// Read Update
+//
+//Route::get('/update', function () {
+//
+//    $updated = DB::update('update posts set title="php with laravel" where id=?',[1]);
+//
+//    return $updated;
+//
+//});
+//
+//
+//Route::get('/delete', function () {
+//
+//    $delete = DB::delete('delete from posts where id=?',[5]);
+//
+//    return $delete;
+//
+//});
 
 
-Route::get('/post/{id}','PostsController@showPost');
+/*
+ -----------------------------------------------------------------------------
+ Database ORM
+-------------------------------------------------------------------------------
+ */
+
+
+//Route::get('/find', function(){
+//
+//    $posts= Post::find(1);
+//
+//
+//    return $posts->title;
+//
+////    foreach($posts as $post) {
+////
+////        return $post->title;
+////    }
+//
+//});
+
+
+//Route::get('/findwhere', function(){
+//
+//    $posts= Post:: where('id=1')->orderBY('id','desc')->take(1)->get();
+//
+//
+//    return $posts->title;
+//
+////    foreach($posts as $post) {
+////
+////        return $post->title;
+////    }
+//
+//});
+
+
+//Route::get('/create', function(){
+//
+//    Post::create(['title'=>'Python','content'=>'Django is common framework for laravel']);
+//
+//
+//});
+
+
+Route::get('/softDelete', function() {
+
+    Post::where('admin',0)->first()->delete();
+
+//    Post::find(1)->delete();
+
+
+});
+
+//Route::get('/readSoftDelete', function() {
+//
+//    $post=Post::withTrashed()->where('id',1)->get();
+//
+//    return $post;
+//
+//});
+
+//Route::get('/restore', function() {
+//
+//    $post=Post::withTrashed()->where('id',3)->restore();
+//
+//    return $post;
+//
+//});
+
+
 
 
 
---
+++ database/migrations/2019_04_21_021423_add_admin_column_to_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdminColumnToPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->integer('admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('admin');
+        });
+    }
+}
